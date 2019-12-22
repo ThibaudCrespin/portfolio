4995ee870b1af82e0e801adddb1b795486fdd2ae
Thibaud Crespin <thibaud.crespin@gmail.com>
2019-12-22 17:55:19 +0100

    last commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,7 +8,11 @@ class Contact extends Component {
     return (
       <Fragment>
         <Details title='Contact'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1>Contact form here</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque purus at risus hendrerit ullamcorper.
+             Sed a erat id massa suscipit viverra mattis nec magna. Vivamus in est interdum, rutrum odio non, rutrum mauris. 
+             Curabitur magna leo, tincidunt nec ullamcorper sed, posuere vel lectus. Quisque nec vehicula turpis. Phasellus sit amet est sem. 
+             Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus ac hendrerit sapien, non dictum neque.</p>
         </Details>
       </Fragment>
     );
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,7 +8,10 @@ class Skills extends Component {
     return (
       <Fragment>
         <Details title='Skills'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque purus at risus hendrerit ullamcorper.
+                Sed a erat id massa suscipit viverra mattis nec magna. Vivamus in est interdum, rutrum odio non, rutrum mauris. 
+                Curabitur magna leo, tincidunt nec ullamcorper sed, posuere vel lectus. Quisque nec vehicula turpis. Phasellus sit amet est sem. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus ac hendrerit sapien, non dictum neque.</p>
         </Details>
       </Fragment>
     );
